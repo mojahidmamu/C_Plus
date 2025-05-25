af7e913e8c5072ecc244388b694b46f508ddf4f9
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-25 10:49:44 +0600

    added

--- Module-01/Min.cpp
+++ Module-01/Min.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include <algorithm> // for build-in-function..
 using namespace std;
 int main()
 {
-    
+    int a, b;
+    cin >> a >> b;
+    cout << min(a, b) << endl; // min = build-in-function
+    cout << max(a, b) << endl; // max = build-in-function
     return 0;
 }
