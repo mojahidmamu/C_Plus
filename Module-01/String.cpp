222195b75752792829d37208211f7799f859caff
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-25 11:12:14 +0600

    added

--- Module-01/String.cpp
+++ Module-01/String.cpp
@@ -1,9 +1,10 @@
+#include<bits/stdc++.h> // for all header-file
 #include <iostream>
 using namespace std;
 int main()
 {
     char str[100];
-    // cin >> str; // without space
+ // cin >> str; // without space
     cin.getline(str, 100); // with space: name, size
     cout << str << endl; 
     return 0;
