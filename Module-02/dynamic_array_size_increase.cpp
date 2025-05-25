eb0dba982628a0fab76a4d35a369a6617f6cd637
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-25 22:46:43 +0600

    added

--- Module-02/dynamic_array_size_increase.cpp
+++ Module-02/dynamic_array_size_increase.cpp
@@ -1,22 +1,24 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int* fun()
+int main()
 {
-     int *a = new int[5];
-    for (int i = 0; i < 5; i++)
+    // int a[3]; // static-array
+    int *a = new int[3]; // dynamic-array
+    for (int i = 0; i < 3; i++)
     {
         cin >> a[i];
     }
-    return a;
-}
-int main()
-{
-    int * x =  fun();
-  
+   int *b = new int[5];
+    for (int i = 0; i < 3; i++)
+    {
+        b[i] = a[i];
+    }
+    b[3] = 50;
+    b[4] = 51;    
     
     for (int i = 0; i < 5; i++)
     {
-        cout << x[i] << " ";
+        cout << b[i] << " ";
     }
     return 0;
 }
