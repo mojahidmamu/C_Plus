a0c506fc2b202f33571295a6c3636f8a998fddf2
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-25 10:02:30 +0600

    added

--- Module-01/EOF.cpp
+++ Module-01/EOF.cpp
@@ -1,6 +1,13 @@
-#include<iostream>
-int main ()
+#include <iostream>
+using namespace std;
+int main()
 {
-    
+    int a;
+
+    while (cin >> a)
+    {
+        cout << a << endl;
+    }
+
     return 0;
 }
