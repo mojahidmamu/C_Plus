4af7ddd1a20c924883e1f12445a2c3be39408c08
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-25 22:21:19 +0600

    added

--- Module-02/Dynamic_array.cpp
+++ Module-02/Dynamic_array.cpp
@@ -1,4 +1,16 @@
-/*
-1. More Memory
-2.  
-*/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a[5];
+    for (int i = 0; i < 5; i++)
+    {
+        cin >> a[i];
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        cout << a[i] << " ";
+    }
+    
+    return 0;
+}
