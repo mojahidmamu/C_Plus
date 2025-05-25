ff818eb751c97e68ab7f214309b0fadb6ba00468
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-25 19:27:09 +0600

    added

--- Module-01/all_practice.cpp
+++ Module-01/all_practice.cpp
@@ -1,14 +1,27 @@
 #include <iostream>
-#include<algorithm>
+#include <algorithm>
+#include <iomanip>
 using namespace std;
 int main()
 {
-    int a =10;
+    int a = 10;
     int b = 20;
 
-    
+        // cout << fixed << setprecision(3) << a;
+
     // cout << max(a, b) << endl; // max & min with build-in-function
     // cout << min(a, b) << endl;
+
+    // swap(a, b);
+    // cout <<  a << endl << a ;
+
+ 
+
+    std::cout << "Before swap: a = " << a << ", b = " << b << std::endl;
+
+    std::swap(a, b);  // Built-in swap function
+
+    std::cout << "After swap: a = " << a << ", b = " << b << std::endl;
 
     // while (cin >> a)
     // {
