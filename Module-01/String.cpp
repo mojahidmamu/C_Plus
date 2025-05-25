a2640b5161d60dbbae3a6901e4ae549e5fc2600e
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-25 10:59:30 +0600

    added

--- Module-01/String.cpp
+++ Module-01/String.cpp
@@ -2,6 +2,9 @@
 using namespace std;
 int main()
 {
-    
+    char str[100];
+    // cin >> str; // without space
+    cin.getline(str, 100); // with space: name, size
+    cout << str << endl; 
     return 0;
 }
