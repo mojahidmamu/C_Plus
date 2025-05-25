e86e3c2a662afbecc4274845d6c5b9b003807148
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-25 10:44:03 +0600

    added

--- Module-01/switch.cpp
+++ Module-01/switch.cpp
@@ -2,6 +2,29 @@
 using namespace std;
 int main()
 {
-    
+    char a;
+    cin >> a;
+    switch (a)
+    {
+    case 'a':
+        cout << "Vowel\n";
+        break;
+    case 'e':
+        cout << "Vowel\n";
+        break;
+    case 'i':
+        cout << "Vowel\n";
+        break;
+    case 'o':
+        cout << "Vowel\n";
+        break;
+    case 'u':
+        cout << "Vowel\n";
+        break;
+        
+    default:
+        cout << "Constant\n";
+        break;
+    }
     return 0;
 }
