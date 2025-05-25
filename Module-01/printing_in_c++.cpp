f244f41e7274fd3e0b1cb615188344b65eeb669c
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-25 09:11:57 +0600

    added

--- Module-01/printing_in_c++.cpp
+++ Module-01/printing_in_c++.cpp
@@ -1,6 +1,10 @@
 #include<iostream>
 int main ()
 {
-    std:: cout << "Mojahid";
+    int a = 10;
+    char b = 'a';
+    double c = 3.1416;
+    std:: cout << a << " " << b << " " << c  << std:: endl;
+    std:: cout << a << " " << b << " " << c  << std:: endl;
     return 0;
 }
