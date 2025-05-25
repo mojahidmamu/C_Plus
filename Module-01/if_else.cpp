bda5186cdd751b1d82fb16beb6217efa67211ece
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-25 10:15:21 +0600

    added

--- Module-01/if_else.cpp
+++ Module-01/if_else.cpp
@@ -1,7 +1,16 @@
-#include<iostream>
-using namespace std: 
+#include <iostream>
+using namespace std;
 int main()
 {
-    
+    int a = 12;
+    if (a % 2 == 0)
+    {
+        cout << "EVEN" << endl;
+    }
+    else
+    {
+        cout << "ODD" << endl;
+    }
+
     return 0;
 }
