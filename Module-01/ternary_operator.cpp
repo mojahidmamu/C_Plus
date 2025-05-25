6b1f78461f8e5f7afb0f4dcb32942f1604ec9468
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-25 10:31:09 +0600

    added

--- Module-01/ternary_operator.cpp
+++ Module-01/ternary_operator.cpp
@@ -1,7 +1,9 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
-    
+    // syntax: condition ? True : False;
+    int a = 12;
+    a % 2 == 0 ? cout << "EVEN" : cout << "ODD" << endl;
     return 0;
 }
