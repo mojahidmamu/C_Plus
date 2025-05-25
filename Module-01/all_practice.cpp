8f443581c68b7c15caa54ca314fc3ea8576cb01a
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-25 19:19:51 +0600

    added

--- Module-01/all_practice.cpp
+++ Module-01/all_practice.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
+#include<algorithm>
 using namespace std;
 int main()
 {
-    int a;
-    while (cin >> a)
-    {
-        cout << a << endl;
-    }
+    int a =10;
+    int b = 20;
+
+    
+    // cout << max(a, b) << endl; // max & min with build-in-function
+    // cout << min(a, b) << endl;
+
+    // while (cin >> a)
+    // {
+    //     cout << a << endl;
+    // }
 
     return 0;
 }
