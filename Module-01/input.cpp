8ffba1560797e6a2f612ea02ca1e131cab1a9d2a
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-25 09:57:01 +0600

    added

--- Module-01/input.cpp
+++ Module-01/input.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
-using namespace std;
+using namespace std; // using namespace
 int main()
 {
-    int a;
-    cin >> a;
-    cout << "this number is: " << a;
+    int a; // same: char, double, long long int etc....
+    cin >> a; // same input all variables type...
+    cout << "this number is: " << a << endl;
+    char c = 'a';
+    cout << (int)c << endl; // typecasting for ASCII value form char,
+    char d = 97;
+    cout << (char)d << endl; 
     return 0;
 }
