5d225b76fd6ecfdaa443e9efeb4d49eb543d5683
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-25 10:53:50 +0600

    added

--- Module-01/Swap.cpp
+++ Module-01/Swap.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include <algorithm> // for build-in-function..
 using namespace std;
 int main()
 {
+    int a, b;
+    cin >> a >> b;
     
+    swap(a, b);
     return 0;
 }
