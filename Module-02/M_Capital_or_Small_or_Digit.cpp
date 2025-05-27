d09c5111b7be6e394b656d014ee18f9bdbc3c3dd
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-27 08:55:32 +0600

    added

--- Module-02/M_Capital_or_Small_or_Digit.cpp
+++ Module-02/M_Capital_or_Small_or_Digit.cpp
@@ -2,6 +2,24 @@
 using namespace std;
 int main()
 {
-    
+    char X;
+    cin >> X;
+    if ('0' <= X && X <= '9')
+    {
+        cout << "IS DIGIT" << endl;
+    }
+    else
+    {
+        cout << "ALPHA" << endl;
+        if ('a' <= X && X <= 'z')
+        {
+            cout << "IS SMALL" << endl;
+        }
+        else
+        {
+            cout << "IS CAPITAL" << endl;
+        }
+    }
+
     return 0;
 }
