237af9cb31bdb9ee3d0719a2c9ac29572bb0758a
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-27 08:49:42 +0600

    added

---
+++ Module-02/C_Simple_Calculator.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    // int add = a + b;
+    // int mul = a * b;
+    // int sub = a - b;
+    cout << a << " + " << b << " = " << a + b << endl;
+    cout << a << " * " << b << " = " << a * b << endl;
+    cout << a << " - " << b << " = " << a - b << endl;
+    return 0;
+}
