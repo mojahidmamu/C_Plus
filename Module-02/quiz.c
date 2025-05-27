864d1a2c5ba323a8ee70e881ae6a9148a7c2acd1
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-27 22:30:07 +0600

    added

---
+++ Module-02/quiz.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    // int a = 10;
+    // int *p = &a;
+    // *p  += 5;
+     
+    // printf("%d", a);
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d ", i);
+        i++;
+    }
+    
+    return 0;
+}
