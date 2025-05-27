dc75c1463fc85230c20944793a8d7727d8d2c3f1
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-27 09:21:58 +0600

    added

--- Module-02/Practice_Problem_02.cpp
+++ Module-02/Practice_Problem_02.cpp
@@ -2,6 +2,23 @@
 using namespace std;
 int main()
 {
+    int N;
+    cin >> N;
+    int A[N];
+    for (int i = 0; i < N; i++)
+    {
+        cin >> A[i];
+    }
+
+    int M;
+    cin >> M;
+    int B[M - N];
+    for (int i = 0; i < M - N; i++)
+    {
+        cin >> B[i];
+    }
+
     
+
     return 0;
 }
