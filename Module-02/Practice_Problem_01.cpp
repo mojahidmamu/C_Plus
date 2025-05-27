288f08455b093e4f8d920204f2329dc575a5f2b0
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-27 09:17:05 +0600

    added

--- Module-02/Practice_Problem_01.cpp
+++ Module-02/Practice_Problem_01.cpp
@@ -1,7 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
+int *get_array(int n)
+{
+    int *arr = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    return arr;
+}
 int main()
 {
-    
+    int N;
+    cin >> N;
+    int *result = get_array(N);
+    for (int i = 0; i < N; i++)
+    {
+        cout << result[i] << " ";
+    }
+
     return 0;
 }
