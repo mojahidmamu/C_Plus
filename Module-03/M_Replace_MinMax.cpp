d2a9ede20420daafe47244050c01c7002ea0881e
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-28 22:07:44 +0600

    added

--- Module-03/M_Replace_MinMax.cpp
+++ Module-03/M_Replace_MinMax.cpp
@@ -2,6 +2,39 @@
 using namespace std;
 int main()
 {
-    
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int min_element = INT_MIN;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] < min_element)
+        {
+            min_element = arr[i];
+        }
+    }
+
+    int max_element = INT_MAX;
+    for (int i = 0; i < n; i++)
+    {
+        // max
+        if (arr[i] > max_element)
+        {
+            max_element = arr[i];
+        }
+    }
+
+    swap(max_element, min_element);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
     return 0;
 }
