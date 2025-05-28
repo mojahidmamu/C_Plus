8ec73a3e539d9bc948135b5ede1c91317b5321e7
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-28 22:01:08 +0600

    added

--- Module-03/J_Lucky_Array.cpp
+++ Module-03/J_Lucky_Array.cpp
@@ -2,6 +2,40 @@
 using namespace std;
 int main()
 {
-    
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int min_element = INT_MIN;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] < min_element)
+        {
+            min_element = arr[i];
+        }
+    }
+
+    int min_count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == min_element)
+        {
+            min_count++;
+        }
+    }
+
+    if (min_count % 2 == 1)
+    {
+        cout << "LUcky" << endl;
+    }
+    else
+    {
+        cout << "Unlucky" << endl;
+    }
+
     return 0;
 }
