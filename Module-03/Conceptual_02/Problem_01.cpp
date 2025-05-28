d0eb7631f717d8d53adbfe89e85556c4d6f42368
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-28 23:00:54 +0600

    added

---
+++ Module-03/Conceptual_02/Problem_01.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Student
+{
+
+    public: 
+    string name;
+    int roll;
+    char section;
+    int math_marks;
+    int cls;
+    
+};
+int main()
+{
+      
+    return 0;
+}
