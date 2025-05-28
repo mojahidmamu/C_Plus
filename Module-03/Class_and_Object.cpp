c6ebcc75e79ce9e11d50e2769be94f829c612109
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-28 07:07:31 +0600

    added

--- Module-03/Class_and_Object.cpp
+++ Module-03/Class_and_Object.cpp
@@ -1,7 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+class Student
+{
+public:
+    char name[100];
+    int roll;
+    double gpa;
+};
+
 int main()
 {
-    
+    Student a;
+    a.roll = 60;
+    a.gpa = 4.83;
+    char temp[100] = "Mojahid";
+    strcpy(a.name, temp);
+
+    cout << a.name << " " << a.gpa << " " << a.roll << endl;
     return 0;
 }
