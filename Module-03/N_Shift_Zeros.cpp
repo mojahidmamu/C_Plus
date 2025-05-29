2c2c064edd4a0ce2f209b6f23687e00173488960
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-29 07:08:05 +0600

    added

--- Module-03/N_Shift_Zeros.cpp
+++ Module-03/N_Shift_Zeros.cpp
@@ -10,5 +10,28 @@ int main()
         cin >> arr[i];
     }
 
+    // logic: 
+    int j = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] != 0)
+        {
+            arr[j] = arr[i];
+            j++;
+        }
+    }
+
+    for (int i = j; i < n; i++)
+    {
+        arr[i] = 0;
+    }
+    
+
+    // print:
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
     return 0;
 }
