d7ee2d593cbc00f3a3ca2d68bd2bef58ee13f0ec
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-29 07:34:25 +0600

    added

--- Module-03/Conceptual_02/Problem_02.cpp
+++ Module-03/Conceptual_02/Problem_02.cpp
@@ -1,7 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main()
+
+class Cricketer
 {
-    
+public:
+    int jersey_no;
+    string country;
+};
+
+int
+main()
+{
+    Cricketer *dhoni = new Cricketer; // dynamic
+
+    dhoni->jersey_no = 7;
+    dhoni->country = "India";
+
+    Cricketer *kohli = new Cricketer;
+    //  kohli = dhoni; // print to dhoni object
+
+    kohli->jersey_no = dhoni->jersey_no;
+    kohli->country = dhoni->country;
+
+    delete dhoni;
+
+    cout << "Kohli " << kohli->jersey_no << " " << kohli->country << endl;
+
+
     return 0;
 }
