9aeb098976263ed8ab9a009c7f5c8cc1bf40e0e8
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-29 07:21:01 +0600

    added

--- Module-03/Conceptual_02/Problem_01.cpp
+++ Module-03/Conceptual_02/Problem_01.cpp
@@ -3,16 +3,41 @@ using namespace std;
 class Student
 {
 
-    public: 
+public:
     string name;
     int roll;
     char section;
     int math_marks;
     int cls;
-    
+
+    Student(string name, int roll, char section, int math_marks, int cls)
+    {
+        this->name = name;
+        this->roll = roll;
+        this->section = section;
+        this->math_marks = math_marks;
+        this->cls = cls;
+    }
 };
 int main()
 {
-      
+
+    Student salman("Salman", 1, 'A', 95, 8);
+    Student atiq("Atiq", 2, 'A', 95, 8);
+    Student afridi("Afridi", 3, 'A', 95, 8);
+
+    if (salman.math_marks > atiq.math_marks && salman.math_marks > afridi.math_marks)
+    {
+        cout << salman.name << endl;
+    }
+    else if (atiq.math_marks > salman.math_marks && atiq.math_marks > afridi.math_marks)
+    {
+        cout << atiq.name << endl;
+    }
+    else
+    {
+        cout << afridi.name << endl;
+    }
+
     return 0;
 }
