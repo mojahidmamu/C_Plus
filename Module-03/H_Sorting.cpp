eaa8641084a0e41c01a2bc9d78bbfc6ef92f3a97
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-29 09:25:28 +0600

    added

--- Module-03/H_Sorting.cpp
+++ Module-03/H_Sorting.cpp
@@ -2,6 +2,20 @@
 using namespace std;
 int main()
 {
-    
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    sort(arr, arr + n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
     return 0;
 }
