cb9fcb111590bd7a197320e260ebf56ed0c3a454
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-29 09:45:32 +0600

    added

--- Module-03/H_Sorting.cpp
+++ Module-03/H_Sorting.cpp
@@ -4,13 +4,27 @@ int main()
 {
     int n;
     cin >> n;
-    int arr[n];
+    int arr[1000];
     for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
     }
 
-    sort(arr, arr + n);
+    //   sort(arr, arr + n); // use build-in-function
+
+ // Bubble Sort: without build-in-function...........
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap A[j] and A[j+1]
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+
 
     for (int i = 0; i < n; i++)
     {
