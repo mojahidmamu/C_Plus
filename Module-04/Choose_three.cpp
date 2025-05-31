355e034fe7cc79202778bbbb76b2bc528c39cbae
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-31 20:10:54 +0600

    added

--- Module-04/Choose_three.cpp
+++ Module-04/Choose_three.cpp
@@ -2,6 +2,32 @@
 using namespace std;
 int main()
 {
-    
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int N, S;
+        cin >> N >> S; // S > for sum
+        int arr[N];
+        for (int i = 0; i < N; i++)
+        {
+            cin >> arr[i];
+        }
+        
+        int i = 0, j = i + 1, k = j + 1;
+        if (arr[i] + arr[j] + arr[k] == S)
+        {
+            cout << "YES" << endl;
+        }
+        // else
+        // {
+        //     cout << "NO" << endl;
+        // }
+
+        i++;
+        j++;
+        k++;
+    }
+
     return 0;
 }
