55ec34e092ed97d9e8a04fd75ca4b95cf3bd328f
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-31 19:04:42 +0600

    added

--- Module-04/Sort_it.cpp
+++ Module-04/Sort_it.cpp
@@ -2,6 +2,30 @@
 using namespace std;
 int main()
 {
-    
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    // Asending:
+    sort(arr, arr + n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    cout << endl;
+
+    // Desending Order:
+    sort(arr, arr + n, greater<int>());
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
     return 0;
 }
+// Accepted
--- Module-04/Sort_it_2.cpp
+++ Module-04/Sort_it_2.cpp
@@ -1,28 +1,28 @@
-#include <bits/stdc++.h>
-using namespace std;
-vector<int> sort_it(int n)
-{
-    vector<int> arr(n);
-    for (int i = 0; i < n; i++)
-    {
-        cin >> arr[i];
-    }
+// #include <bits/stdc++.h>
+// using namespace std;
+// vector<int> sort_it(int n)
+// {
+//     vector<int> arr(n);
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> arr[i];
+//     }
 
-    sort(arr.begin(), arr.end(), greater<int>());
+//     sort(arr.begin(), arr.end(), greater<int>());
 
-    return arr;
-}
-int main()
-{
-    int n;
-    cin >> n;
+//     return arr;
+// }
+// int main()
+// {
+//     int n;
+//     cin >> n;
 
-    vector<int> result = sort_it(n);
-    for (int i = 0; i < n; i++)
-    {
-        cout << result[i] << " ";
-    }
-    cout << endl;
+//     vector<int> result = sort_it(n);
+//     for (int i = 0; i < n; i++)
+//     {
+//         cout << result[i] << " ";
+//     }
+//     cout << endl;
 
-    return 0;
-}
+//     return 0;
+// }
