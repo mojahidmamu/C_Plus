d64314d8ef5a8ecbcd8dadb8a608e6601979f7c4
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-01 18:19:32 +0600

    added

--- Module-05/String_capacity.cpp
+++ Module-05/String_capacity.cpp
@@ -4,11 +4,13 @@ int main()
 {
     // s.size() -> returns the size of the string.
     string s = "Mojahid";
-    cout << s.size() << endl; // size for 
-    cout << s.max_size() << endl; 
-    cout << s.capacity() << endl; 
-    // cout  << s.clear() << endl;
+    // cout << s.size() << endl;  
+    // cout << s.max_size() << endl; 
+    // cout << s.capacity() << endl; 
+    // s.clear();
+    // cout << s;
     cout  << s.empty() << endl;
-    // cout  << s.resize(12) << endl;
+    s.resize(4); // first 4 letter/character
+    cout << s << endl;
     return 0;
 }
