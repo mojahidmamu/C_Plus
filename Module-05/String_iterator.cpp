b0b4dfe36da3d8c09ef4acc90cda261466263235
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-01 19:58:13 +0600

    added

--- Module-05/String_iterator.cpp
+++ Module-05/String_iterator.cpp
@@ -2,6 +2,17 @@
 using namespace std;
 int main()
 {
+     string s = "Adu";
+    //  string s2 = "Mojahid";
+
+    // for (int i = 0; i < s.size(); i++)
+    // {
+    //     cout << s[i] << endl;
+    // }
     
+    s.begin();
+    cout << s << endl;
+    s.end();
+    cout << s << endl;
     return 0;
 }
