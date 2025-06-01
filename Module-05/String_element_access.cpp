dd0509d528e7dc8b383bddebf48d65f399d618a7
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-01 18:22:25 +0600

    added

--- Module-05/String_element_access.cpp
+++ Module-05/String_element_access.cpp
@@ -2,6 +2,11 @@
 using namespace std;
 int main()
 {
-    
+    string s = "Mojahid";
+
+    // cout << s[0] << endl; 
+    // cout << s.at(0) << endl; 
+    // cout << s.back() << endl;  // last element
+    cout << s.front() << endl;  // first element
     return 0;
 }
