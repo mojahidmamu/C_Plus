da59d7edc9570cbab57f6427f4be4241a3e8371e
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-01 19:06:23 +0600

    added

--- Module-05/String_Modifier.cpp
+++ Module-05/String_Modifier.cpp
@@ -9,7 +9,8 @@ int main()
     // s.pop_back();
 
     // s.erase(3, 2); // delete from index-3 and only 2 element
-    s.replace(3, 2, "MA"); // replace from index-3 and 2 element and replace 'MA'
+    // s.replace(3, 2, "MA"); // replace from index-3 and 2 element and replace 'MA'
+    // s.insert(8,"Mojahid"); // insert a full string from index-8
 
 
      cout << s << endl;
