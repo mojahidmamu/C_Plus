a9594ddab385dc5f44403721f6f8a2a5935ef31b
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-01 19:03:22 +0600

    added

--- Module-05/String_Modifier.cpp
+++ Module-05/String_Modifier.cpp
@@ -2,6 +2,16 @@
 using namespace std;
 int main()
 {
-    
+     string s = "Abdullah";
+    //  string s2 = "Mojahid";
+    //  s.append(s2);
+    // s.push_back('A');
+    // s.pop_back();
+
+    // s.erase(3, 2); // delete from index-3 and only 2 element
+    s.replace(3, 2, "MA"); // replace from index-3 and 2 element and replace 'MA'
+
+
+     cout << s << endl;
     return 0;
 }
--- Module-05/String_element_access.cpp
+++ Module-05/String_element_access.cpp
@@ -4,8 +4,8 @@ int main()
 {
     string s = "Mojahid";
 
-    // cout << s[0] << endl; 
-    // cout << s.at(0) << endl; 
+    // cout << s[0] << endl; // specific index
+    // cout << s.at(0) << endl;  // specific index
     // cout << s.back() << endl;  // last element
     cout << s.front() << endl;  // first element
     return 0;
