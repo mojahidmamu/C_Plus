208be3ee0e4f3f5bee669c1f029f7995b65e3ddd
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-01 19:15:13 +0600

    added

--- Module-04/new.cpp
+++ Module-04/new.cpp
@@ -2,6 +2,49 @@
 using namespace std;
 int main()
 {
-    
+    int testCase;
+    cin >> testCase;
+    while (testCase--)
+    {
+        int n, sum;
+        cin >> n >> sum; // sum > for sum
+        int arr[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+
+        int found = 0;
+
+        int i = 0, j = i + 1, k = j + 1;
+        for (int i = 0; i < n - 2; i++)
+        {
+            for (int j = i + 1; j < n - 1; j++)
+            {
+                for (int k = j + 1; k < n; k++)
+                {
+                    if (arr[i] + arr[j] + arr[k] == sum)
+                    {
+                        found = 1;
+                        break;
+                    }
+                }
+            }
+        }
+
+         
+
+        if (found == 1)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+
+    }
+
     return 0;
 }
+// Accepted...
