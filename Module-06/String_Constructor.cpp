8a683588922bfc2bf1a069d788ec76ff4461fead
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-02 10:23:42 +0600

    added

--- Module-06/String_Constructor.cpp
+++ Module-06/String_Constructor.cpp
@@ -2,6 +2,17 @@
 using namespace std;
 int main()
 {
-    
+    // string  s= "Mojahid";
+    // 1
+    // string s("Mojahid");
+    // 2
+    // string s("Abdullah Mojahid", 5); // first 5 element
+    // 3
+    // string s = "Abdullah Mojahid";
+    // string t(s, 5); // without first 5 element
+    // 4
+    // string s(5, 'M'); // MMMMMM > output
+
+    // cout << s << endl;
     return 0;
 }
