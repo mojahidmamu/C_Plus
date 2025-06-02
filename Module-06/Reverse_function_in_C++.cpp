89a3a93f4bd6544af42346790cfe5578347580c5
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-02 10:43:47 +0600

    added

--- Module-06/Reverse_function_in_C++.cpp
+++ Module-06/Reverse_function_in_C++.cpp
@@ -1,19 +1,31 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     int arr[n];
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> arr[i];
+//     }
+
+//     reverse(arr, arr+n);
+//     for (int i = 0; i < n; i++)
+//     {
+//         cout << arr[i] << " ";
+//     }
+//     return 0;
+// }
+
+
+// For a string: 
 #include <bits/stdc++.h>
 using namespace std;
 int main()
 {
-    int n;
-    cin >> n;
-    int arr[n];
-    for (int i = 0; i < n; i++)
-    {
-        cin >> arr[i];
-    }
-
-    reverse(arr, arr+n);
-    for (int i = 0; i < n; i++)
-    {
-        cout << arr[i] << " ";
-    }
+    string s = "MOjahid";
+    reverse(s.begin(), s.end());
+    cout << s << endl;
     return 0;
 }
