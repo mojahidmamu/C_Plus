96db69c0777be7286a2dac066ef81e9dcab3b027
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-03 05:52:53 +0600

    added

---
+++ Module-06/I_Palindrome.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cin >> s;
+
+
+    
+    int palirdrome = 1;
+    if (palirdrome == 1)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
