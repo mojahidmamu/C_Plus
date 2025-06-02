2da1d0249731bcfca923203f20e760579c597a71
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-02 12:19:32 +0600

    added

--- Module-06/Function_inside_class.cpp
+++ Module-06/Function_inside_class.cpp
@@ -2,14 +2,23 @@
 using namespace std;
 class Student
 {
-    public:
-    
-
+public:
+    string name;
+    Student(string name)
+    {
+        this->name = name;
+    }
+    void hello()
+    {
+        cout << "Hello from " << name << endl;
+    }
 };
-
 
 int main()
 {
-    
+    Student sakib("Shakib Ahmed");
+    sakib.hello();
+    Student Mojahid("Abdullah all Mojahid");
+    Mojahid.hello();
     return 0;
 }
