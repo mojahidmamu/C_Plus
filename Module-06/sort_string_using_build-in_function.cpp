4c4eb874e447e473037542bf3737357a4c9c91d1
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-02 10:30:50 +0600

    added

---
+++ Module-06/sort_string_using_build-in_function.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cin >> s;
+    
+    sort(s.begin(), s.end()); // sort from a-z
+    cout << s << endl;
+    return 0;
+}
