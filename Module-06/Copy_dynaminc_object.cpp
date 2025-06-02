dc224143aadc24488f15626039abfbb37dfeae47
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-02 12:33:36 +0600

    added

--- Module-06/Copy_dynaminc_object.cpp
+++ Module-06/Copy_dynaminc_object.cpp
@@ -1,7 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
+class Cricketer
+{
+public:
+    string country;
+    int jersey;
+    Cricketer(string country, int jersey)
+    {
+        this->country = country;
+        this->jersey = jersey;
+    }
+};
 int main()
 {
+    Cricketer *dhoni = new Cricketer("India", 7);
+    Cricketer *kholi = new Cricketer("India", 18);
+    kholi = dhoni;
+    cout << "Dhoni " << dhoni->country << " " << dhoni->jersey << endl;
+    // delete dhoni;
     
+    cout << "Kholi " << kholi->country << " " << kholi->jersey << endl;
     return 0;
 }
