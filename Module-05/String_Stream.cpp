b444a805d8e60c1887e1b196c79e80b64e0d6fde
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-02 07:15:39 +0600

    added

--- Module-05/String_Stream.cpp
+++ Module-05/String_Stream.cpp
@@ -1,11 +1,37 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     string s;
+//     getline(cin, s);
+//     cout << s << endl;
+
+//     stringstream ss(s);
+//     string word;
+//     ss >> word;
+//     cout << word << endl;
+//      ss >> word;
+//     cout << word << endl;
+//      ss >> word;
+//     cout << word << endl;
+//      ss >> word;
+//     cout << word << endl;
+//     return 0;
+// }
+
 #include <bits/stdc++.h>
 using namespace std;
 int main()
 {
-    string s;
-    getline(cin, s);
-    cout << s << endl;
+    string input = "123 456 789";
+    stringstream ss(input);
 
-    stringstream ss(s);
+    int a, b , c;
+    ss >> a >> b >> c;
+
+    cout << "a:" << a << endl;
+    cout << "b:" << b << endl;
+    cout << "c:" << c << endl;
+  
     return 0;
 }
