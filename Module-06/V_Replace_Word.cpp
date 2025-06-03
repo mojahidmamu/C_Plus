9abf29c3479cb4a73447bb125e8b1f29a14db33d
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-03 06:44:16 +0600

    added

---
+++ Module-06/V_Replace_Word.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cin >> s;
+
+    size_t pos;
+    while ((pos = s.find("EGYPT")) != string::npos)
+    {
+        s.replace(pos, 5, " ");
+    }
+
+    cout << s << endl;
+    return 0;
+}
