318b73f5f272504ec2b2f7f497ce3b1cb9dc12e2
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-03 08:32:19 +0600

    added

--- Module-06/B_ICPC_Balloons.cpp
+++ Module-06/B_ICPC_Balloons.cpp
@@ -2,6 +2,32 @@
 using namespace std;
 int main()
 {
-    
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+
+        set<char> solved;
+
+        int total_ballons = 0;
+        for (char c : s)
+        {
+            if (solved.find(c) == solved.end())
+            {
+                total_ballons += 2;
+                solved.insert(c);
+            }
+            else
+            {
+                total_ballons += 1;
+            }
+        }
+        cout << total_ballons << endl;
+    }
+
     return 0;
 }
