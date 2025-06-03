8b4951aa3a2a03f9c894f16f579b99dee49df987
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-03 06:54:30 +0600

    added

--- Module-06/A_Lucky.cpp
+++ Module-06/A_Lucky.cpp
@@ -2,6 +2,30 @@
 using namespace std;
 int main()
 {
-    
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        string ticket;
+        cin >> ticket;
+
+        int sumFirst = 0;
+        int sumLast = 0;
+        for (int i = 0; i < 3; i++)
+        {
+            sumFirst += ticket[i] - '0';
+            sumLast += ticket[i + 3] - '0';
+        }
+
+        if (sumFirst == sumLast)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+
     return 0;
 }
