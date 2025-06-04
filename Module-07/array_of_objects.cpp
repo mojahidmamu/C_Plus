9290296fcd9147683b446ac6267a5b6539009c99
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-04 14:16:43 +0600

    added

--- Module-07/array_of_objects.cpp
+++ Module-07/array_of_objects.cpp
@@ -1,7 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
+class Student 
+{
+    public:
+    string name;
+    int roll;
+    int marks;
+};
 int main()
 {
+    // int a[10]
+    int n;
+    cin >> n;
+    Student arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i].name >> arr[i].marks >> arr[i].roll;
+    }
     
     return 0;
 }
