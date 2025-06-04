d95826fd04de2402ef6afd410cd877bf7a12616a
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-04 14:49:11 +0600

    added

--- Module-07/sort_array_of_objects.cpp
+++ Module-07/sort_array_of_objects.cpp
@@ -1,7 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
+class Student
+{
+public:
+    string name;
+    int roll;
+    int marks;
+};
 int main()
 {
-    
+    int n;
+    cin >> n;
+    Student arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i].name >> arr[i].roll >> arr[i].marks;
+    }
+
+    int mx= INT_MIN; // for maximum
+    //  int minimum = INT_MAX; // for minimum
+    for (int i = 0; i < n; i++)
+    {
+         mx = max(arr[i].marks, mx); // for maximum
+        //  minimum = min(arr[i].marks, minimum); //for minimum
+    }
+    cout << " Maximum marks is: " <<  mx << endl; // for maximum
+    // cout << " Minimum marks is: " <<  minimum << endl; // for minimum
+
     return 0;
 }
