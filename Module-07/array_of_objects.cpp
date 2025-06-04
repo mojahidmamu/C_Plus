50133b0a51a3572af320bba0867fb718dff5074b
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-04 14:37:03 +0600

    added

--- Module-07/array_of_objects.cpp
+++ Module-07/array_of_objects.cpp
@@ -1,11 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
-class Student 
+class Student
 {
-    public:
+public:
     string name;
     int roll;
     int marks;
+    // Student(string name, int roll, int makrs)
+    // {
+    //     string name = name ;
+    //     int roll = roll;
+    //     int marks = marks;
+    // }
 };
 int main()
 {
@@ -16,13 +22,15 @@ int main()
 
     for (int i = 0; i < n; i++)
     {
+        // cin.ignore(); // for ignore gurbase value.......
+        // getline(cin, arr[i].name) // this is only for input string with space
         cin >> arr[i].name >> arr[i].roll >> arr[i].marks;
     }
 
-     for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
-        cout << arr[i].name << " " << arr[i].roll << " " <<  arr[i].marks << " ";
+        cout << arr[i].name << " " << arr[i].roll << " " << arr[i].marks << " ";
     }
-    
+
     return 0;
 }
