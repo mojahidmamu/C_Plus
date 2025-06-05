49af91b1e9bf07f13be0b3bfac520b16429d567b
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-05 09:15:25 +0600

    added

--- Module-07/Practice_Problem_03.cpp
+++ Module-07/Practice_Problem_03.cpp
@@ -2,6 +2,24 @@
 using namespace std;
 int main()
 {
-    
+    string S;
+    string X;
+    getline(cin, S); // input the sentence
+    cin >> X;        // input the word
+
+    stringstream ss(S);
+    string word;
+
+    int count = 0;
+    while (ss >> word)
+    {
+        if (word == X)
+        {
+            count++;
+        }
+    }
+
+    cout << count << endl;
+
     return 0;
 }
