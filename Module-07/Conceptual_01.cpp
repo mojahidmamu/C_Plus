598183ac0604507721a19f17544bbc227ce8ba4c
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-06 16:47:21 +0600

    added

--- Module-07/Conceptual_01.cpp
+++ Module-07/Conceptual_01.cpp
@@ -2,6 +2,30 @@
 using namespace std;
 int main()
 {
+    string s;
+    // cin >> s;// is not for a sentence...
+    getline(cin, s); // for a sentence
+
+    // int len = s.size();
+    // cout << len << endl;
+    // insert: function
+    // cout << s.front() << endl;
+    // replace: function
+    // erase: function
+    // s.erase();
+    // s.find("gram");
+
+    // for (int i = 0; i < len; i++)
+    // {
+    //     cout << s[i] << " ";
+    // }
+
+    stringstream ss(s);
+    string word;
     
+    ss >> word;
+
+    cout << word << endl;
+
     return 0;
 }
