57a739ff983f7d25091075ade6bb2de9500b0e35
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-09 09:19:29 +0600

    added

---
+++ Module-06/only_string.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+
+{
+
+ // string s="hello world"; // আমরা নরমালি এভাবে এসাইন করি স্ট্রিং। 
+
+ string s("hello world"); // স্ট্রিং ক্লাসকে কল করে কন্সট্রাক্টরকে একটি স্ট্রিং দিয়ে দিচ্ছি। কন্সট্রাক্টর সেটি স্ট্রিং s এর মধ্যে এসাইন করে দিচ্ছে। 
+
+ cout<<s<<endl;
+
+ return 0;
+
+}
