86943a8a6fca5dc87507e3eb57af4f02019d8285
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-12 20:29:48 +0600

    added

--- Module-08/Find_jassica.cpp
+++ Module-08/Find_jassica.cpp
@@ -2,6 +2,28 @@
 using namespace std;
 int main()
 {
-    
+    string s;
+    getline(cin, s);
+    stringstream ss(s);
+    string word;
+    bool jassicaIsHere = false;
+    while (ss >> word)
+    {
+        if (word == "Jessica")
+        {
+            jassicaIsHere = true;
+            break;
+        }
+    }
+
+    if (jassicaIsHere)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+
     return 0;
 }
