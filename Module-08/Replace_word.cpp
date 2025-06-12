003a26ed19546e5b4718c24ace91613e9d207351
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-12 20:41:23 +0600

    added

--- Module-08/Replace_word.cpp
+++ Module-08/Replace_word.cpp
@@ -2,6 +2,22 @@
 using namespace std;
 int main()
 {
+    int test;
+    cin >> test;
+    while (test--)
+    {
+        string str, target_str;
+        cin >> str, target_str;
+
+        stringstream ss(str);
+        string word;
+        while (ss >> word)
+        {
+            
+        }
+        
+
+    }
     
     return 0;
 }
