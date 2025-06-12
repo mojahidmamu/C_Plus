2e75b92abe00bd90bd643917ec8b59edc28fe4c8
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-12 20:38:42 +0600

    added

--- Module-08/Reverse_it.cpp
+++ Module-08/Reverse_it.cpp
@@ -1,7 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
+class Student
+{
+public:
+    string name;
+    int cls;
+    char section;
+    int id;
+};
+
 int main()
 {
-    
+    int n;
+    cin >> n;
+    Student arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i].name >> arr[i].cls >> arr[i].section >> arr[i].id;
+    }
+
+    // reverse(arr->section, arr->section + n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i].name << " " << arr[i].cls << " " << arr[i].section << " " << arr[i].id << endl;
+    }
+
     return 0;
 }
