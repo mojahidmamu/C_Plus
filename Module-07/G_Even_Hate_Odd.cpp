968d156f117e30cad24e322cf8078ded80eca61f
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-06-16 08:13:22 +0600

    added

--- Module-07/G_Even_Hate_Odd.cpp
+++ Module-07/G_Even_Hate_Odd.cpp
@@ -2,6 +2,24 @@
 using namespace std;
 int main()
 {
+    int testcase;
+    cin >> testcase;
+    while (testcase--)
+    {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            cout << arr[i] << " " ;
+        }
+        cout << endl;
+    }
     
     return 0;
 }
